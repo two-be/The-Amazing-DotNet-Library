2fe934ba1e93dc2ce7bc73ba34575602a78c4977
The Amazing Developer-Man <Two.Logician@icloud.com>
2017-07-03 12:16:57 +0700

    first commit

---
+++ Extensions/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softveloper.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToSha512(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return string.Empty;
+            }
+
+            using (var sha = new SHA512Managed())
+            {
+                var utf8Bytes = Encoding.UTF8.GetBytes(value);
+                var hash = sha.ComputeHash(utf8Bytes);
+                return BitConverter.ToString(hash).Replace("-", string.Empty);
+            }
+        }
+    }
+}
