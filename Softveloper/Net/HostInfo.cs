b972ee6dbcab4f127e7344ef3ecb5675e33c7a28
The Amazing Developer-Man <Two.Logician@icloud.com>
2017-10-16 09:38:55 +0700

    Solution

---
+++ Softveloper/Net/HostInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softveloper.Net
+{
+    public class HostInfo
+    {
+        public static string GetIpAddressV4()
+        {
+            var hostName = Dns.GetHostName();
+            return GetIpAddressV4(hostName);
+        }
+
+        public static string GetIpAddressV4(string hostName)
+        {
+            var hostInfo = Dns.GetHostEntry(hostName);
+            var ipAddress = hostInfo.AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+            return ipAddress?.ToString() ?? string.Empty;
+        }
+    }
+}
