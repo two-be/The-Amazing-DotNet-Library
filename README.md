560b21be97de047c511b7d204fca53e25ae2096a
Two <two@The-Amazing-MacBook-Pro.local>
2018-02-15 13:35:56 +0700

    httpRequest.GetStringValue("key")

--- README.md
+++ README.md
@@ -11,6 +11,10 @@ Filerectory.DeleteFile("./FileName");
 ```c#
 using Softveloper.Extensions;
 
+// HttpRequest Get String Value
+var request = Request; //HttpRequest
+var value = request.GetStringValue("key");
+
 // SHA512
 var password = "Password";
 var sha512 = password.ToSha512();
---
+++ Softveloper/Extensions/HttpRequestExtendsions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Softveloper.Extensions
+{
+    public static class HttpRequestExtendsions
+    {
+        public static string GetStringValue(this HttpRequest request, string key)
+        {
+            StringValues values = string.Empty;
+            var success = request.Form.TryGetValue(key, out values);
+            if (success)
+            {
+                var value = string.Join(string.Empty, values);
+                return value;
+            }
+            return string.Empty;
+        }
+    }
+}
